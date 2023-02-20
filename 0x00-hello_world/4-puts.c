3d55c33fea8c60f1a0bae754830faadd84b6d4f1
mandyjay <jaymandy71@gmail.com>
2023-02-20 08:04:16 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/**
+ * main - A program that prints a line with the puts function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}

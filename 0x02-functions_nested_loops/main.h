f41e01667787a53c0a1470f85f6d8ccaec99ea21
mandyjay <jaymandy71@gmail.com>
2023-02-25 16:20:25 -0800

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,6 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int n);
-void jact_bauer(void);
+void jack_bauer(void);
 void times_table(void);
 void add(int,  int);
